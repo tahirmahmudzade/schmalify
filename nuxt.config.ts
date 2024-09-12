3ab4134e827a022a17968503319b06f331e0a677
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-09-12 20:28:38 +0200

    nuxthub config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxthub/core'],
+  ui: {
+    global: true,
+  },
+  hub: {
+    database: true,
+    blob: true,
+    kv: true,
+    remote: true,
+  },
+  image: {
+    dir: 'public',
+  },
+  devtools: { enabled: true },
+  imports: {
+    dirs: ['composables/**'],
+  },
+  compatibilityDate: '2024-09-12',
 })
