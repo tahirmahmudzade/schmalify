ac6c243d965c27ca18c06f751c89e45016d4dcf6
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-09-13 09:17:25 +0200

    add device module for device detection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/image', '@nuxthub/core'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxthub/core',
+    '@nuxtjs/device',
+  ],
   ui: {
     global: true,
   },
