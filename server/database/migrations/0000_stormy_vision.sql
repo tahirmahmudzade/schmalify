b4decd9656823b897cc76d9257757fd5c1a258b7
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-09-18 07:01:58 +0200

    update db

--- server/database/migrations/0000_stormy_vision.sql
+++ server/database/migrations/0000_stormy_vision.sql
@@ -5,14 +5,6 @@ CREATE TABLE `category` (
 	`created_at` text DEFAULT (datetime('now'))
 );
 --> statement-breakpoint
-CREATE TABLE `guest` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`first_name` text,
-	`last_name` text,
-	`phone` text NOT NULL,
-	`created_at` text DEFAULT (datetime('now'))
-);
---> statement-breakpoint
 CREATE TABLE `item` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text(20) NOT NULL,
@@ -20,32 +12,30 @@ CREATE TABLE `item` (
 	`price` integer NOT NULL,
 	`category_id` text,
 	`seller_id` text,
-	`guest_id` integer,
 	`condition` text,
 	`status` text DEFAULT 'available',
 	`created_at` text DEFAULT (datetime('now')),
 	FOREIGN KEY (`category_id`) REFERENCES `category`(`id`) ON UPDATE cascade ON DELETE set null,
-	FOREIGN KEY (`seller_id`) REFERENCES `user`(`id`) ON UPDATE cascade ON DELETE cascade,
-	FOREIGN KEY (`guest_id`) REFERENCES `guest`(`id`) ON UPDATE cascade ON DELETE cascade
+	FOREIGN KEY (`seller_id`) REFERENCES `user`(`id`) ON UPDATE cascade ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `user` (
 	`id` text PRIMARY KEY NOT NULL,
-	`email` text NOT NULL,
+	`email` text,
 	`first_name` text,
 	`last_name` text,
-	`password` text NOT NULL,
-	`username` text NOT NULL,
+	`password` text,
+	`username` text,
 	`location` text,
 	`avatar` text,
 	`phone` text,
 	`admin` integer DEFAULT false,
+	`is_guest` integer DEFAULT false,
 	`created_at` text DEFAULT (datetime('now'))
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `category_id_unique` ON `category` (`id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `category_name_unique` ON `category` (`name`);--> statement-breakpoint
-CREATE UNIQUE INDEX `guest_id_unique` ON `guest` (`id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `item_id_unique` ON `item` (`id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `user_id_unique` ON `user` (`id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);
