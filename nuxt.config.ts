817c4ebb5d51de671a5c669e383ff1c594872b97
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-09-23 19:26:02 +0200

    update deps & formatting & app configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,24 @@
+// @ts-ignore
+const isDev = import.meta.env.MODE === 'development'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/image',
-    '@nuxthub/core',
-    '@nuxtjs/device',
-    '@nuxt/icon',
-    'nuxt-auth-utils',
-  ],
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxthub/core', '@nuxtjs/device', '@nuxt/icon', 'nuxt-auth-utils'],
+
   app: {
-    pageTransition: {
-      name: 'page',
-      mode: 'out-in',
-    },
-    layoutTransition: {
-      name: 'layout',
-      mode: 'out-in',
-    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
   },
+
   css: ['~/assets/css/transitions.css'],
-  ui: {
-    global: true,
-  },
-  $development: {
-    hub: {
-      database: true,
-      blob: true,
-      kv: true,
-      remote: true,
-    },
-  },
-  hub: {
-    database: true,
-    blob: true,
-    kv: true,
-    remote: true,
-  },
-  image: {
-    dir: 'public',
-  },
+  ui: { global: true },
+  image: { dir: 'public' },
+
+  $development: { hub: { database: true, blob: true, kv: true, remote: true } },
+  hub: { database: true, blob: true, kv: true, remote: true },
+
   devtools: { enabled: true },
-  imports: {
-    dirs: ['composables/**'],
-  },
+  imports: { dirs: ['composables/**'] },
+  vue: { propsDestructure: true },
   compatibilityDate: '2024-09-12',
 })
