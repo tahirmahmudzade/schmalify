a18f2c050ada1bee1be5fe2d42f51d3d978f0f23
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-09-30 19:17:31 +0200

    default layout -v 3.0.0

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
-import type { HorizontalNavigationLink, DropdownItem } from '#ui/types'
-
-const route = useRoute()
+import type { DropdownItem } from '#ui/types'
 const { loggedIn, user } = useUserSession()
+
+const uiHeader = reactive({
+  container: 'flex items-center justify-between h-auto py-2', // Added vertical padding
+  left: 'flex-shrink-0 flex items-center gap-1.5',
+  center: 'flex-grow flex-shrink min-w-0 mx-2',
+  right: 'flex-shrink-0 flex items-center gap-1.5',
+  logo: 'flex-shrink-0 font-bold text-xl text-gray-900 dark:text-white flex items-end gap-1.5 w-32 sm:w-40',
+})
 
 const items: DropdownItem[][] = [
   [
@@ -68,95 +74,31 @@ const items: DropdownItem[][] = [
   ],
 ]
 
-const links = computed<HorizontalNavigationLink[] | HorizontalNavigationLink[][]>(() => [
-  [
-    {
-      label: 'Schmalify',
-      to: '/',
-    },
-    route.path !== '/'
-      ? {
-          label: '',
-          as: 'Search',
-        }
-      : {
-          label: '',
-        },
-  ],
-  [
-    {
-      label: 'Sell Now',
-      click: () => {
-        !loggedIn.value ? useSellNowNotificationModal() : useCreateLettingModal()
-      },
-    },
-    {
-      label: 'Profile',
-    },
-  ],
-])
+function handleSellNowClick() {
+  !loggedIn.value ? useSellNowNotificationModal() : useCreateLettingModal()
+}
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <!-- Added container with mx-auto and padding -->
-    <UHorizontalNavigation
-      :links="links"
-      :ui="{
-        wrapper: 'fixed top-0 left-0 w-full bg-gray-900 z-50 shadow-lg mx-auto px-5', // Solid background color and shadow
-        after: '',
-        before: 'hover:before:bg-gray-900 dark:hover:before:bg-gray-900',
-        inactive: 'text-gray-400 dark:text-gray-400 font-bold transition-colors duration-300', // add transition here
-        active: 'text-gray-400 dark:text-gray-400 font-bold transition-colors duration-300', // add transition here
-      }"
-    >
-      <template #default="{ link }">
-        <span class="relative">
-          <NuxtImg v-if="link.label === 'Schmalify'" width="150" height="50" src="img/main-logo.png" />
-          <div v-if="route.path !== '/' && link.as === 'Search'">
-            <div class="flex-1">
-              <div class="relative">
-                <span class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <svg
-                    class="h-6 w-6 text-gray-900"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-4.35-4.35m1.86-5.86a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
-                </span>
-                <input
-                  type="text"
-                  class="w-full py-2 pl-10 pr-4 rounded-full focus:outline-none bg-gray-100 text-gray-900"
-                  placeholder="Search..."
-                />
-              </div>
-            </div>
-          </div>
-          <div v-if="link.label === 'Sell Now'">
-            <ClientOnly>
-              <ButtonsSellNowButton :click="link.click as () => void" />
-            </ClientOnly>
-          </div>
-          <div v-else-if="link.label === 'Profile'">
-            <ClientOnly>
-              <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-                <ButtonsProfileButton />
-              </UDropdown>
-            </ClientOnly>
-          </div>
-        </span>
+  <div>
+    <UHeader :ui="uiHeader">
+      <template #logo>
+        <NuxtImg width="150" height="50" src="img/main-logo.png" />
       </template>
-    </UHorizontalNavigation>
-    <div class="mt-20">
-      <slot />
-    </div>
+
+      <template #center>
+        <SearchInput icon-size="1.2rem" />
+      </template>
+
+      <template #right>
+        <!-- <UColorModeButton /> -->
+        <ButtonsSellNowButton :click="handleSellNowClick" />
+        <UDropdown :items="items" :popper="{ placement: 'bottom' }">
+          <ButtonsProfileButton />
+        </UDropdown>
+      </template>
+    </UHeader>
+
+    <slot />
   </div>
 </template>
