2d3bbab504f377f9fb563bf99e51cce11fff4222
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-02 23:46:47 +0200

    fix cloudflare icon bundle issue & dependency cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,26 +12,19 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
   ],
 
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' },
-  },
+  app: { pageTransition: { name: 'page', mode: 'out-in' }, layoutTransition: { name: 'layout', mode: 'out-in' } },
 
   css: ['~/assets/css/transitions.css'],
   ui: { global: true },
   image: { dir: 'public' },
+  icon: { clientBundle: { scan: true, sizeLimitKb: 256 } },
 
   runtimeConfig: {
     mailgunApiKey: process.env.MAILGUN_API_KEY,
     mailgunDomain: process.env.MAILGUN_DOMAIN,
     mailgunFromEmail: process.env.MAILGUN_FROM_EMAIL,
     jwtSecret: process.env.JWT_SECRET,
-    oauth: {
-      google: {
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      },
-    },
+    oauth: { google: { clientId: process.env.GOOGLE_CLIENT_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET } },
   },
 
   $development: { hub: { database: true, blob: true, kv: true, remote: true } },
