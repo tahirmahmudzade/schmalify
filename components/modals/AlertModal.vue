34855f22c9e02d32d766b0bec00bdfd1278755b7
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-06 11:07:58 +0200

    update actions modals

---
+++ components/modals/AlertModal.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { ButtonColor } from '#ui/types'
+const emits = defineEmits(['close'])
+const {
+  title = 'Alert',
+  description = '',
+  confirmLabel = 'Confirm',
+  confirmColor = 'gray',
+  confirmAction = () => {
+    navigateTo('/')
+  },
+} = defineProps<{
+  title: string
+  description: string
+  confirmLabel?: string
+  confirmColor?: ButtonColor
+  confirmAction?: () => void
+}>()
+
+function onClose() {
+  emits('close')
+}
+</script>
+
+<template>
+  <UModal>
+    <UCard class="p-6 text-center">
+      <!-- Centered Alert Icon -->
+      <div class="text-yellow-500 text-5xl mb-4">
+        <Icon name="i-material-symbols-warning-outline" size="4rem" />
+      </div>
+      <!-- Title and Description -->
+      <h3 class="text-xl font-semibold mb-2">{{ title }}</h3>
+      <p class="text-gray-400 mb-6">{{ description }}</p>
+      <!-- Close Button -->
+      <div class="flex justify-center space-x-4">
+        <UButton label="Close" color="gray" class="px-4 py-2 rounded-md" @click="onClose" />
+        <UButton :label="confirmLabel" :color="confirmColor" class="px-4 py-2 rounded-md" @click="confirmAction()" />
+      </div>
+    </UCard>
+  </UModal>
+</template>
--- components/modals/ConfirmModal.vue
+++ components/modals/ConfirmModal.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 const emits = defineEmits(['close', 'confirm'])
-const { title = 'Confirm your action.', description = '' } = defineProps<{
-  title: string
-  description: string
-}>()
+const { title = 'Confirm your action.', description = '' } = defineProps<{ title: string; description: string }>()
 
 function onClose() {
   emits('close')
@@ -21,11 +18,9 @@ function onConfirm() {
       <div class="text-red-500 text-5xl mb-4">
         <Icon name="i-material-symbols-cancel-outline" size="4rem" />
       </div>
-
       <!-- Title and Description -->
       <h3 class="text-xl font-semibold mb-2">{{ title }}</h3>
       <p class="text-gray-400 mb-6">{{ description }}</p>
-
       <!-- Buttons Section -->
       <div class="flex justify-center space-x-4">
         <UButton label="Cancel" color="gray" class="px-4 py-2 rounded-md" @click="onClose" />
