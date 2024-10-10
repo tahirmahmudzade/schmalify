56a60f6de0d29445a989c2027608a07fc6910f7b
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-10 19:00:41 +0200

    fix create listing as guest

--- composables/useCreateLettingModal.ts
+++ composables/useCreateLettingModal.ts
@@ -5,10 +5,24 @@ export const useCreateLettingModal = async (asGuest: boolean = false) => {
   const modal = useModal()
   const { user } = useUserSession()
 
-  if (user.value && user.value.isGuest) {
+  // Fetch categories once and use them later
+  const data = await $fetch('/api/category')
+
+  // If user is not logged in, allow them to create a letting directly
+  if (!user.value) {
+    modal.open(CreateLettingModal, {
+      categories: data.categories,
+      asGuest,
+      onClose: () => modal.close(),
+    })
+    return
+  }
+
+  // If user is logged in and is a guest
+  if (user.value.isGuest) {
     modal.open(AlertModal, {
       title: 'Limited Access',
-      description: 'Guests can only create one listing, please click on sign in as user to create more listings.',
+      description: 'Guests can only create one listing. Please sign in as a user to create more listings.',
       confirmLabel: 'Sign in as a user',
       confirmColor: 'green',
       confirmAction() {
@@ -17,37 +31,33 @@ export const useCreateLettingModal = async (asGuest: boolean = false) => {
           useRegisterModal()
         }, 500)
       },
-      onClose: () => {
-        modal.close()
-      },
+      onClose: () => modal.close(),
     })
-  } else {
-    const { phone } = await $fetch(`/api/users/${user.value?.id}/phone`)
-
-    if (!phone) {
-      modal.open(AlertModal, {
-        title: 'Phone Number Required',
-        description: 'Please add a phone number to your account before creating a listing.',
-        confirmLabel: 'Add Phone Number',
-        confirmColor: 'green',
-        confirmAction() {
-          modal.close()
-          navigateTo('/profile')
-        },
-        onClose: () => {
-          modal.close()
-        },
-      })
-    } else {
-      const data = await $fetch('/api/category')
-
-      modal.open(CreateLettingModal, {
-        categories: data.categories,
-        asGuest,
-        onClose: () => {
-          modal.close()
-        },
-      })
-    }
+    return
   }
+
+  // If user is logged in and is not a guest, check for a phone number
+  const { phone } = await $fetch(`/api/users/${user.value.id}/phone`)
+
+  if (!phone) {
+    modal.open(AlertModal, {
+      title: 'Phone Number Required',
+      description: 'Please add a phone number to your account before creating a listing.',
+      confirmLabel: 'Add Phone Number',
+      confirmColor: 'green',
+      confirmAction() {
+        modal.close()
+        navigateTo('/profile')
+      },
+      onClose: () => modal.close(),
+    })
+    return
+  }
+
+  // If user has a phone number, allow them to create a letting
+  modal.open(CreateLettingModal, {
+    categories: data.categories,
+    asGuest,
+    onClose: () => modal.close(),
+  })
 }
