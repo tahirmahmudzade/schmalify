6a7f1bf171cd12c90ae60f98d2c107878ae3d970
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-10 12:55:00 +0200

    no items compt modifications

--- components/NoItems.vue
+++ components/NoItems.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-const { title = 'No items listed here yet, click sell now to create your listing', subtitle = '' } = defineProps<{
+const {
+  title = 'No items listed here yet, click sell now to create your listing',
+  subtitle = '',
+  showRecommendations = false,
+} = defineProps<{
   title?: string
   subtitle?: string
+  showRecommendations?: boolean
 }>()
 
 const { loggedIn, user } = useUserSession()
@@ -15,6 +20,12 @@ function handleSellNowClick() {
   <div class="flex flex-col items-center justify-center text-center py-10 text-white">
     <h2 class="text-xl md:text-2xl font-semibold">{{ title }}</h2>
     <p v-if="subtitle" class="text-base md:text-lg text-gray-400">{{ subtitle }}</p>
-    <ButtonsSellNowButton class="mt-5" button-size="md" :is-label-hidden="false" :click="handleSellNowClick" />
+    <ButtonsSellNowButton
+      v-if="showRecommendations"
+      class="mt-5"
+      button-size="md"
+      :is-label-hidden="false"
+      :click="handleSellNowClick"
+    />
   </div>
 </template>
