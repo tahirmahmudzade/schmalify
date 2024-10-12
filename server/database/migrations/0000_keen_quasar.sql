2b3691f235bb72f2a23e8812d6066edbb843178d
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-12 19:34:41 +0200

    new db structure

---
+++ server/database/migrations/0000_keen_quasar.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `category` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`img` text,
+	`created_at` text DEFAULT (datetime('now'))
+);
+--> statement-breakpoint
+CREATE TABLE `item` (
+	`id` text PRIMARY KEY NOT NULL,
+	`title` text(35) NOT NULL,
+	`description` text(400),
+	`image` text DEFAULT 'default-item.webp',
+	`price` integer NOT NULL,
+	`category_id` text,
+	`seller_id` text,
+	`condition` text,
+	`status` text DEFAULT 'available',
+	`created_at` text DEFAULT (datetime('now')),
+	`pending` integer DEFAULT true,
+	FOREIGN KEY (`category_id`) REFERENCES `category`(`id`) ON UPDATE cascade ON DELETE set null,
+	FOREIGN KEY (`seller_id`) REFERENCES `user`(`id`) ON UPDATE cascade ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` text PRIMARY KEY NOT NULL,
+	`email` text,
+	`first_name` text,
+	`last_name` text,
+	`password` text,
+	`username` text,
+	`location` text,
+	`avatar` text,
+	`phone` text,
+	`admin` integer DEFAULT false,
+	`is_guest` integer DEFAULT false,
+	`created_at` text DEFAULT (datetime('now'))
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `category_id_unique` ON `category` (`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `category_name_unique` ON `category` (`name`);--> statement-breakpoint
+CREATE UNIQUE INDEX `item_id_unique` ON `item` (`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `user_id_unique` ON `user` (`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);
