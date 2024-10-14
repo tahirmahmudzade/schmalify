1e77f84932df9834d9182a5b9fd32653fd5c5ed8
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-14 10:01:38 +0200

    testing cf header css issue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,74 +2,74 @@
 import type { DropdownItem } from '#ui/types'
 const { loggedIn, user } = useUserSession()
 
-const path = computed(() => useRoute().path)
+// const path = computed(() => useRoute().path)
 
-const items: DropdownItem[][] = [
-  [
-    loggedIn.value
-      ? {
-          label: 'Profile',
-          icon: 'i-healthicons-ui-user-profile',
-          iconClass: 'text-green-500 dark:text-green-500',
-          click: () => {
-            navigateTo('/profile')
-          },
-        }
-      : {
-          label: 'Log in',
-          icon: 'i-ri-login-circle-line',
-          iconClass: 'text-green-500 dark:text-green-500',
-          click: () => {
-            useLoginModal()
-          },
-        },
-    user.value && user.value.isGuest
-      ? {
-          label: 'Sign in as user',
-          icon: 'i-ri-login-circle-line',
-          iconClass: 'text-green-500 dark:text-green-500',
-          click: () => {
-            useRegisterModal()
-          },
-        }
-      : {
-          label: '',
-          class: 'hidden',
-          disabled: true,
-        },
-  ],
-  [
-    {
-      label: 'Listings',
-      icon: 'i-material-symbols-receipt-long',
-      iconClass: 'text-orange-500 dark:text-orange-500',
-      click: () => {
-        navigateTo('/profile/listings')
-      },
-      disabled: !loggedIn.value,
-    },
-  ],
-  [
-    {
-      label: 'Log out',
-      icon: 'i-ri-logout-circle-line',
-      iconClass: 'text-red-500 dark:text-red-500',
-      disabled: !loggedIn.value,
-      click: () => {
-        useLogout()
-      },
-    },
-  ],
-]
+// const items: DropdownItem[][] = [
+//   [
+//     loggedIn.value
+//       ? {
+//           label: 'Profile',
+//           icon: 'i-healthicons-ui-user-profile',
+//           iconClass: 'text-green-500 dark:text-green-500',
+//           click: () => {
+//             navigateTo('/profile')
+//           },
+//         }
+//       : {
+//           label: 'Log in',
+//           icon: 'i-ri-login-circle-line',
+//           iconClass: 'text-green-500 dark:text-green-500',
+//           click: () => {
+//             useLoginModal()
+//           },
+//         },
+//     user.value && user.value.isGuest
+//       ? {
+//           label: 'Sign in as user',
+//           icon: 'i-ri-login-circle-line',
+//           iconClass: 'text-green-500 dark:text-green-500',
+//           click: () => {
+//             useRegisterModal()
+//           },
+//         }
+//       : {
+//           label: '',
+//           class: 'hidden',
+//           disabled: true,
+//         },
+//   ],
+//   [
+//     {
+//       label: 'Listings',
+//       icon: 'i-material-symbols-receipt-long',
+//       iconClass: 'text-orange-500 dark:text-orange-500',
+//       click: () => {
+//         navigateTo('/profile/listings')
+//       },
+//       disabled: !loggedIn.value,
+//     },
+//   ],
+//   [
+//     {
+//       label: 'Log out',
+//       icon: 'i-ri-logout-circle-line',
+//       iconClass: 'text-red-500 dark:text-red-500',
+//       disabled: !loggedIn.value,
+//       click: () => {
+//         useLogout()
+//       },
+//     },
+//   ],
+// ]
 
-function handleSellNowClick() {
-  !loggedIn.value && !user.value ? useSellNowNotificationModal() : useCreateLettingModal()
-}
+// function handleSellNowClick() {
+//   !loggedIn.value && !user.value ? useSellNowNotificationModal() : useCreateLettingModal()
+// }
 </script>
 
 <template>
   <div>
-    <UHeader
+    <!-- <UHeader
       :ui="{
         wrapper: 'bg-background border-b border-gray-800 -mb-px sticky top-0 z-50 bg-gray-900',
         container: 'flex items-center justify-between h-auto py-2', // Added vertical padding
@@ -78,23 +78,23 @@ function handleSellNowClick() {
         right: 'flex-shrink-0 flex items-center gap-1.5',
         logo: 'flex-shrink-0 font-bold text-xl text-gray-900 dark:text-white flex items-end gap-1.5 w-32 sm:w-40',
       }"
-    >
-      <template #logo>
+    > -->
+    <!-- <template #logo>
         <NuxtImg width="150" height="50" src="img/main-logo.png" />
-      </template>
+      </template> -->
 
-      <template #center>
+    <!-- <template #center>
         <SearchInput :class="path === '/' ? 'hidden' : ''" :is-on-header="true" text-size="xs" icon-size="1.2rem" />
-      </template>
+      </template> -->
 
-      <template #right>
-        <!-- <UColorModeButton /> -->
-        <ButtonsSellNowButton :click="handleSellNowClick" />
+    <!-- <template #right> -->
+    <!-- <UColorModeButton /> -->
+    <!-- <ButtonsSellNowButton :click="handleSellNowClick" />
         <UDropdown :items="items" :popper="{ placement: 'bottom' }">
           <ButtonsProfileButton />
         </UDropdown>
-      </template>
-    </UHeader>
+      </template> -->
+    <!-- </UHeader> -->
 
     <slot />
   </div>
