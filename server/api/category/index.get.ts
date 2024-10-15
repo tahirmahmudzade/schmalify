9a4b86d742eaf76740869552c005560e7d71db4a
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-15 13:33:23 +0200

    modifications on category api

--- server/api/category/index.get.ts
+++ server/api/category/index.get.ts
@@ -6,9 +6,11 @@ export default defineCachedEventHandler(
     const categories = await getAllCategories()
 
     categories.sort((a, b) => {
-      if (a.name === 'Free') return -1
+      if (a.name === 'Free') return -1 // 'Free' goes to the start
       if (b.name === 'Free') return 1
-      return a.name.localeCompare(b.name) // Alphabetical sorting
+      if (a.name === 'Other') return 1 // 'Other' goes to the end
+      if (b.name === 'Other') return -1
+      return a.name.localeCompare(b.name) // Alphabetical sorting for the rest
     })
 
     return { statusCode: 200, categories: categories.map(c => ({ ...c, id: encodeId(c.id) })) }
