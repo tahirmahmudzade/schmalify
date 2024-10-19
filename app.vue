e3673e2ae81fcc8cd7f1c191af368a3c799caea2
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-19 09:43:04 +0200

    remove pwa from project

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <VitePwaManifest /> -->
   <NuxtLoadingIndicator />
   <NuxtRouteAnnouncer politeness="polite">
     <template #default="{ message }">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
     'nuxt-typed-router',
     '@pinia/nuxt',
     '@vueuse/nuxt',
-    '@vite-pwa/nuxt',
   ],
 
   experimental: { payloadExtraction: true, appManifest: true },
@@ -36,28 +35,6 @@ export default defineNuxtConfig({
 
   $development: { hub: { database: true, blob: true, kv: true, remote: true } },
   hub: { database: true, blob: true, kv: true, remote: true },
-
-  // pwa: {
-  //   strategies: sw ? 'injectManifest' : 'generateSW',
-  //   registerType: 'autoUpdate',
-  //   client: { installPrompt: true },
-  //   manifest: {
-  //     name: 'Schmalify',
-  //     short_name: 'Schmalify',
-  //     description:
-  //       'Schmalify is a second-hand marketplace designed for students in Schmalkalden, providing a dedicated platform to buy and sell items within the local student community. Schmalify aims to simplify the trading process and foster a more efficient way for students to connect and exchange goods.',
-  //     background_color: '#111827',
-  //     theme_color: '#111827',
-  //     icons: [
-  //       { src: 'icons/icon_64x64.png', sizes: '64x64', type: 'image/png' },
-  //       { src: 'icons/icon_144x144.png', sizes: '144x144', type: 'image/png' },
-  //       { src: 'icons/icon_192x192.png', sizes: '192x192', type: 'image/png' },
-  //       { src: 'icons/icon_512x512.png', sizes: '512x512', type: 'image/png' },
-  //     ],
-  //   },
-  //   workbox: { globPatterns: ['**/*.{js,css,html,png,svg,ico}'] },
-  //   injectManifest: { globPatterns: ['**/*.{js,css,html,png,svg,ico}'] },
-  // },
 
   devtools: { enabled: true },
   imports: { dirs: ['composables/**'] },
