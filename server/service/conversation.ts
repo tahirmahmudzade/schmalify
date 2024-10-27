f1d5151ebe6d2cea8e0029226a1146cdec20265d
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-27 22:59:15 +0100

    modify conversation service

--- server/service/conversation.ts
+++ server/service/conversation.ts
@@ -16,3 +16,18 @@ export function getConversationById(conversationId: string): Promise<Conversatio
 export function createConversation(conversationData: CreateConversation) {
   return useDrizzle().insert(tables.conversation).values(conversationData)
 }
+
+export function getUserConversations(
+  userId: string,
+): Promise<{ id: string; participants: string[] | null; lastUpdated: string | null }[]> {
+  return useDrizzle()
+    .select()
+    .from(tables.conversation)
+    .where(
+      sql`EXISTS (
+        SELECT 1
+        FROM json_each(${tables.conversation.participants})
+        WHERE json_each.value = ${userId}
+      )`,
+    )
+}
