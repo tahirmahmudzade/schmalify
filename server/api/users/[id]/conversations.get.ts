1f5f74f2e0e5c78460495d057d1b356010b9d717
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-10-27 22:59:52 +0100

    add conversation & token user apis for messaging

---
+++ server/api/users/[id]/conversations.get.ts
@@ -0,0 +1,30 @@
+import { Conversation } from '~/server/database/drizzle'
+import { getUserConversations } from '~/server/service/conversation'
+import { getUserById } from '~/server/service/user'
+
+export default defineEventHandler(async (event): Promise<{ statusCode: number; conversations: Conversation[] }> => {
+  await requireUserSession(event)
+
+  const paramId = getRouterParam(event, 'id')
+
+  if (!paramId) {
+    throw createError({ statusCode: 400, message: 'id is required' })
+  }
+
+  const decodedUserId = decodeId(paramId)
+
+  const user = await getUserById(decodedUserId)
+
+  if (!user) {
+    throw createError({ statusCode: 404, message: 'User not found' })
+  }
+
+  const conversations: Conversation[] = await getUserConversations(user.id)
+
+  console.log('conversations', conversations)
+
+  return {
+    statusCode: 200,
+    conversations: conversations.map(c => ({ ...c, participants: c.participants!.map(encodeId) })),
+  }
+})
---
+++ server/api/users/token.get.ts
@@ -0,0 +1,12 @@
+import { sign } from '@tsndr/cloudflare-worker-jwt'
+
+export default defineEventHandler(async (event): Promise<{ tempToken: string }> => {
+  const { user } = await requireUserSession(event)
+
+  const tempToken = await sign(
+    { userId: user.id, exp: Math.floor(Date.now() / 1000) + 15 * 60 },
+    process.env.JWT_SECRET || 'prvscret',
+  )
+
+  return { tempToken }
+})
