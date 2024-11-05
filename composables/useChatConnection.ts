eec0ce1a6ee80ab476f2dcf907b62ed5208f7ec0
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-11-05 15:43:37 +0100

    fix: websocket connection on prod & protocols

--- composables/useChatConnection.ts
+++ composables/useChatConnection.ts
@@ -1,15 +1,16 @@
-const protocol = process.env.NODE_ENV === 'production' ? 'wss' : 'ws'
-const host = process.env.NODE_ENV === 'production' ? 'schmalify.com' : 'localhost:3000'
-console.log('node env: ', process.env.NODE_ENV)
+const protocol = location.protocol === 'https:' ? 'wss' : 'ws'
+console.log('location.protocol: ', location.protocol)
+
+const host = location.host
+
+console.log('protocol: ', protocol)
+console.log('host: ', host)
 
 export function useChatConnection<T>(conversationId: string, tempToken: string, immediate = false) {
-  const { user } = useUserSession()
   const { $i18n } = useNuxtApp()
   const t = $i18n.t
 
-  if (!user.value) {
-    throw createError({ statusCode: 401, message: t("You're not logged in, please log in to chat") })
-  }
+  console.log('laoding chat connection')
 
   return useWebSocket<T>(
     `${protocol}://${host}/api/messages/websocket?conversationId=${conversationId}&token=${tempToken}`,
--- server/api/messages/websocket.ts
+++ server/api/messages/websocket.ts
@@ -4,12 +4,20 @@ import { getConversationById } from '~/server/service/conversation'
 
 export default defineWebSocketHandler({
   async open(peer) {
+    console.log('inside websocket')
     const parsedUrl = parseURL(peer.url)
     const queryParams = getQuery(parsedUrl.search)
     const conversationId = queryParams.conversationId as string | undefined
     const token = queryParams.token as string | undefined
 
+    console.log('parsedUrl: ', parsedUrl)
+    console.log('queryParams: ', queryParams)
+    console.log('conversationId: ', conversationId)
+    console.log('token: ', token)
+
     if (!conversationId || !token) {
+      console.log('Missing conversationId or token')
+
       throw createError({ statusCode: 400, message: 'Missing conversationId' })
     }
 
@@ -23,20 +31,27 @@ export default defineWebSocketHandler({
         | undefined
 
       if (!decoded) {
+        console.log('Invalid token')
+
         throw createError({ statusCode: 401, message: 'Invalid token' })
       }
 
       userId = decoded.payload.userId
+
+      console.log('userId: ', userId)
     } catch (error) {
       console.error('Error verifying token:', error)
       throw createError({ statusCode: 401, message: 'Invalid token' })
     }
 
     const conversation = await getConversationById(decodedConversationId)
+    console.log('conversation: ', conversation)
 
     const decodedUserId = decodeId(userId)
 
     if (!conversation || !conversation.participants?.includes(decodedUserId)) {
+      console.log('Conversation not found')
+
       throw createError({ statusCode: 404, message: 'Conversation not found' })
     }
 
@@ -45,6 +60,8 @@ export default defineWebSocketHandler({
     const otherParticipantId = conversation.participants.find(id => id !== decodedUserId)
 
     if (!otherParticipantId) {
+      console.log('Other participant not found in conversation')
+
       throw createError({ statusCode: 404, message: 'Other participant not found in conversation' })
     }
 
@@ -52,14 +69,35 @@ export default defineWebSocketHandler({
     peer.ctx.conversationId = conversationId // encoded conversationId
     peer.ctx.room = `chat:${conversationId}` // room name with encoded conversationId
     peer.ctx.receiverId = encodeId(otherParticipantId) // encoded receiverId
+    console.log('peer.ctx: ', peer.ctx)
+
+    console.log('subscribing')
 
     peer.subscribe(peer.ctx.room)
+    console.log('User joined room subscribe', peer.ctx.room)
+
+    console.log('sending join message')
+
+    peer.publish(peer.ctx.room, {
+      content: 'User joined the chat',
+      senderId: 'server',
+      receiverId: peer.ctx.receiverId,
+      timestamp: new Date().toISOString(),
+    }) // send join message to room
+
+    console.log('User joined room', peer.ctx.room)
   },
   close(peer) {
     const room = peer.ctx.room // room name with encoded conversationId
     const userId = peer.ctx.userId // encoded userId
 
     if (room) {
+      peer.publish(room, {
+        content: 'User left the chat',
+        senderId: 'server',
+        receiverId: peer.ctx.receiverId,
+        timestamp: new Date().toISOString(),
+      }) // send leave message to room
       peer.unsubscribe(room)
       console.log(`User ${userId} left room ${room}`)
     } else {
