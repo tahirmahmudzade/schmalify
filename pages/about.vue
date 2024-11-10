acb51b0fe8952065fa81d6d1b62d435c168416fe
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-11-10 23:40:04 +0100

    update about us image src

--- pages/about.vue
+++ pages/about.vue
@@ -6,38 +6,38 @@ const team = [
     name: 'Tahir Mahmudzade',
     role: 'Founder / Developer',
     description: 'Full Stack Developer with a passion for building products that people love.',
-    imageUrl: '/img/team/tahir_mahmudzade.jpeg',
+    imageUrl: '/img/team/tahir_mahmudzade.webp',
   },
   {
     name: 'Baris Eroglu',
     role: 'QA Engineer',
     description: 'I test it, perfect it, so you love it.',
-    imageUrl: '/img/team/baris_eroglu.png',
+    imageUrl: '/img/team/baris_eroglu.webp',
   },
   {
     name: 'Ismail Mahili',
     role: 'AI Engineer',
     description: 'A passionate software developer focused on building innovative AI solutions using Python.',
-    imageUrl: '/img/team/ismail_mahili.jpg',
+    imageUrl: '/img/team/ismail_mahili.webp',
   },
   {
     name: 'Kamila Mammadzada',
     role: 'UI/UX Designer',
     description:
       'Crafting intuitive and visually engaging digital experiences that connect users effortlessly with technology.',
-    imageUrl: '/img/team/kamila_mamedzade.jpeg',
+    imageUrl: '/img/team/kamila_mamedzade.webp',
   },
   {
     name: 'Fahmin Mahili',
     role: 'AI Engineer',
     description: 'Innovating with machine learning and AI technologies to create impactful real-world solutions.',
-    imageUrl: '/img/team/fahmin_mahili.jpeg',
+    imageUrl: '/img/team/fahmin_mahili.webp',
   },
   {
     name: 'Oleksii Muzyka',
     role: 'UI/UX Designer',
     description: 'UI/UX Designer, responsible for user friendly design and marketing',
-    imageUrl: '/img/team/oleksii_muzyka.jpeg',
+    imageUrl: '/img/team/oleksii_muzyka.webp',
   },
 ]
 
@@ -55,7 +55,7 @@ const teamDescription =
     </div>
     <ul role="list" class="mx-auto mt-12 grid gap-x-8 gap-y-16 text-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       <li v-for="person in team" :key="person.name" class="flex flex-col items-center">
-        <NuxtImg :src="person.imageUrl" alt="profile" class="h-24 w-24 rounded-full mx-auto object-cover" format="webp" />
+        <NuxtImg :src="person.imageUrl" alt="profile" class="h-24 w-24 rounded-full mx-auto object-cover" />
         <h3 class="mt-4 text-base font-semibold leading-7 tracking-tight text-white">{{ person.name }}</h3>
         <p class="text-sm leading-6 text-gray-400">{{ t(person.role) }}</p>
         <p class="mt-2 text-sm leading-6 text-gray-500 max-w-xs">{{ t(person.description) }}</p>
